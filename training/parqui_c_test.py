c73d5275dc22836bfc716a6235185bac805e8d5d
Claire <cparquier@gmail.com>
2016-02-10 22:59:16 +0100

    work filling 1x1 squares. Score = 106612

--- training/parqui_c_test.py
+++ training/parqui_c_test.py
@@ -12,16 +12,16 @@ with open(sys.argv[1]) as fp:
       dims = line.split()
     else:
       lines.append(line)
-      print(len(lines))
-    print(line)
-  fp.close()
-
-print("THE BEGINNING")
-for i in dims:
-  print(i)
-print("THE END")
 
 # paint!
 
-for l in range(int(dims[0])):
-  sys.stdout.write(str(l))
+text_to_write = ""
+nb_lines = 0
+for r in range(int(dims[0])):
+  for c in range(int(dims[1])):
+    if lines[r][c] == '#':
+      text_to_write += "PAINT_SQUARE {} {} 0\n".format(r, c)
+      nb_lines += 1
+with open(sys.argv[1].split('.')[0] + "_output.txt", 'w') as f_out:
+  f_out.write(str(nb_lines) + "\n" + text_to_write)
+
