67bc23f39211f08d23a010f0704e92f752320b78
Kaci Adjou <adjou_k@epita.fr>
2016-02-11 22:08:21 +0100

    Fixing nearest_warehouse, add a target warehouse in parameter

--- nearest_warehouse.py
+++ nearest_warehouse.py
@@ -8,7 +8,7 @@ from parse import *
 import sys
 from distance import *
 
-def next_warehouses(drone, order, warehouses):
+def next_warehouses(drone, order, warehouses, origin_warehouse):
   useful_warehouses = []
   distances = []
   for w in warehouses:
@@ -16,10 +16,10 @@ def next_warehouses(drone, order, warehouses):
       if (i > j):
         useful_warehouses.append(w)
   for ware in useful_warehouses:
-    distances.append(get_distance(ware, warehouses[0]))
+    distances.append(get_distance(ware, origin_warehouse))
   return useful_warehouses[distances.index(min(distances))]
 
 if __name__ == '__main__':
   infos = parse(sys.argv[1])
-  a = next_warehouses(infos.drones[0], infos.orders[0], infos.warehouses)
+  a = next_warehouses(infos.drones[0], infos.orders[0], infos.warehouses, info.warehouses[0])
   print("line: {}, col: {}".format(a.line, a.col))
