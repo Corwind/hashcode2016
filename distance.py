707b5c941f3bf64a3b1299b64eb17df9e0305006
Guillaume Doré <dore_g@epita.fr>
2016-02-11 21:11:33 +0100

    improved get_distance to use objects instead of coordinates

--- distance.py
+++ distance.py
@@ -2,8 +2,5 @@
 
 from math import *
 
-def get_distance(ra, rb, ca, cb):
-    if ra < 0 or rb < 0 or ca < 0 or cb < 0:
-        print('COORDONNÉE NÉGATIVE WESH')
-        return -1
-    return ceil(sqrt(pow(abs(ra-rb), 2) + pow(abs(ca-cb), 2)))
+def get_distance(o1, o2):
+    return ceil(sqrt(pow(abs(o1.line - o2.line), 2) + pow(abs(o1.col - o2.col), 2)))
