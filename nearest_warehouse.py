21a8682808e2070438cf9519968333c103db986c
Kaci Adjou <adjou_k@epita.fr>
2016-02-11 21:26:13 +0100

    Add nearest warehouse function

---
+++ nearest_warehouse.py
@@ -0,0 +1,25 @@
+#! /usr/bin/python3
+
+"""
+  Written by adjou_k
+"""
+
+from parse import *
+import sys
+from distance import *
+
+def next_warehouses(drone, order, warehouses):
+  useful_warehouses = []
+  distances = []
+  for w in warehouses:
+    for i, j in zip(order.products, w.stock):
+      if (i > j):
+        useful_warehouses.append(w)
+  for ware in useful_warehouses:
+    distances.append(get_distance(ware, warehouses[0]))
+  return useful_warehouses[distances.index(min(distances))]
+
+if __name__ == '__main__':
+  infos = parse(sys.argv[1])
+  a = next_warehouses(infos.drones[0], infos.orders[0], infos.warehouses)
+  print("line: {}, col: {}".format(a.line, a.col))
