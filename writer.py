2af6ff04eeecdf194024b7bf6d6a65d78c5ebd27
Claire <cparquier@gmail.com>
2016-02-11 20:01:12 +0100

    writer with usefull functions done

---
+++ writer.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+import sys
+import re
+
+nb_commands = 0
+res = []
+res.append([0, ""])#nb_commands, commands
+
+def load(drone_number, nb_products, type_product, warehouse_number):
+  res[len(res) - 1][1] += "{} L {} {} {}".format(drone_number, warehouse_number, type_product, nb_products)
+  res[len(res) - 1][0] += 1
+
+def unload(drone_number, nb_products, type_product, warehouse_number):
+  res[len(res) - 1][1] += "{} U {} {} {}".format(drone_number, warehouse_number, type_product, nb_products)
+  res[len(res) - 1][0] += 1
+
+def deliver(drone_number, nb_products, type_product, customer_number):
+  res[len(res) - 1][1] += "{} D {} {} {}".format(drone_number, customer_number, type_product, nb_products)
+  res[len(res) - 1][0] += 1
+
+def wait(drone_number, nb_turns_to_wait):
+  res[len(res) - 1][1] += "{} W {}".format(drone_number, nb_turns_to_wait)
+  res[len(res) - 1][0] += 1
+
+def write():
+  with open(sys.argv[1].split('.')[0] + "_output.txt", 'w') as f_out:
+    for n, s in res:
+      print(n)
+    best = min(res, key=lambda r: r[0])
+    f_out.write(str(best[0]) + "\n" + best[1])
