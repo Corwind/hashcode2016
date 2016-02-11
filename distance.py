9073f81cdcfca896eed1b3b648d630e06c934309
Erwan MILIS <erwan.milis@gmail.com>
2016-02-11 02:11:42 +0100

    Alert if there's a negative coordinate

--- distance.py
+++ distance.py
@@ -3,4 +3,7 @@
 from math import *
 
 def get_distance(ra, rb, ca, cb):
+    if ra < 0 or rb < 0 or ca < 0 or cb < 0:
+        print('COORDONNÉE NÉGATIVE WESH')
+        return -1
     return ceil(sqrt(pow(abs(ra-rb), 2) + pow(abs(ca-cb), 2)))
