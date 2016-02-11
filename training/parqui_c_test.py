a76fd5ea132bea6c38fc050d0860f605d8bce282
Claire <cparquier@gmail.com>
2016-02-11 18:33:33 +0100

    big squares

--- training/parqui_c_test.py
+++ training/parqui_c_test.py
@@ -1,6 +1,7 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 import sys
 import re
+from copy import deepcopy
 
 # parser
 
@@ -11,17 +12,50 @@ with open(sys.argv[1]) as fp:
     if dims == []:
       dims = line.split()
     else:
-      lines.append(line)
+      lines.append(list(line))
 
 # paint!
 
-text_to_write = ""
-nb_lines = 0
+res = []
+
+res.append([0, ""])
 for r in range(int(dims[0])):
   for c in range(int(dims[1])):
     if lines[r][c] == '#':
-      text_to_write += "PAINT_SQUARE {} {} 0\n".format(r, c)
-      nb_lines += 1
-with open(sys.argv[1].split('.')[0] + "_output.txt", 'w') as f_out:
-  f_out.write(str(nb_lines) + "\n" + text_to_write)
+      res[len(res)-1][1] += "PAINT_SQUARE {} {} 0\n".format(r, c)
+      res[len(res)-1][0] += 1
 
+def check_square(liness, r_center, c_center, size):
+  for r in range(r_center - size, r_center + size + 1):
+    for c in range(c_center - size, c_center + size + 1):
+      if r < 0 or c < 0 or r > int(dims[0]) - 1 or c > int(dims[1]) - 1:
+        return False
+      if liness[r][c] == ".":
+        return False
+  return True
+
+def paint_square(liness, r_center, c_center, size):
+  for r in range(r_center - size, r_center + size + 1):
+    for c in range(c_center - size, c_center + size + 1):
+      liness[r][c] = '.'
+
+res.append([0, ""])
+liness = deepcopy(lines)
+for r in range(int(dims[0])):
+  for c in range(int(dims[1])):
+    if liness[r][c] == '#':
+      square_size = 0
+      while check_square(liness, r + square_size + 1, c + square_size + 1, square_size + 1):
+        square_size += 1
+
+      res[len(res)-1][1] += "PAINT_SQUARE {} {} {}\n".format(r + square_size, c + square_size, square_size)
+      paint_square(liness, r + square_size, c + square_size, square_size)
+      res[len(res)-1][0] += 1
+
+
+
+with open(sys.argv[1].split('.')[0] + "_output.txt", 'w') as f_out:
+  for n, s in res:
+    print(n)
+  best = min(res, key=lambda r: r[0])
+  f_out.write(str(best[0]) + "\n" + best[1])
